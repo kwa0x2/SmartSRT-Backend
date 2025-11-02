90684730a3b7a12e7d9a9a8a270a673c3afb2cfd
Alper Karakoyun <alperenibo14@gmail.com>
2025-11-02 17:06:30 +0000

    test: test 5

--- api/middleware/paddle_middleware.go
+++ api/middleware/paddle_middleware.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"bytes"
-	"io"
 	"net/http"
 
 	"github.com/PaddleHQ/paddle-go-sdk/v3"
@@ -13,41 +11,19 @@ func PaddleWebhookVerifier(secretKey string) gin.HandlerFunc {
 	verifier := paddle.NewWebhookVerifier(secretKey)
 
 	return func(c *gin.Context) {
-		bodyBytes, err := io.ReadAll(c.Request.Body)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Failed to read request body"})
-			return
-		}
-
-		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
-
-		verifyReq := c.Request.Clone(c.Request.Context())
-		verifyReq.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
-
 		verified := false
+
 		handler := verifier.Middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			verified = true
 		}))
 
-		responseRecorder := &responseWriter{ResponseWriter: c.Writer, statusCode: http.StatusOK}
-		handler.ServeHTTP(responseRecorder, verifyReq)
+		handler.ServeHTTP(c.Writer, c.Request)
 
-		if !verified || responseRecorder.statusCode != http.StatusOK {
+		if !verified {
 			c.Abort()
 			return
 		}
 
-		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 		c.Next()
 	}
-}
-
-type responseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
 }
